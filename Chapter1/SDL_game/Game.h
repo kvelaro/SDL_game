b608b788501424a128be1d2adae89c8ceea1cc89
velaro <kamilmiem@gmail.com>
2019-05-28 11:05:09 +0300

    Chapter1

---
+++ Chapter1/SDL_game/Game.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef __GAME__
+#define __GAME__
+
+#include <SDL.h>
+
+class Game
+{
+public:
+	bool init(const char *title, int xpos, int ypos, int height, int width, int flags);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running();
+
+private:
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+
+	bool m_bRunning;
+};
+
+#endif // !__GAME__
+
